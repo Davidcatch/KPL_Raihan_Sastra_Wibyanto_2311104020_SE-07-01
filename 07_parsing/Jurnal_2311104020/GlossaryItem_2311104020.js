16ca4a5c4d3c0e5bea324ce8d6d683d7c77e960f
DavidCatch <raihanwibyanto@gmail.com>
2025-04-26 15:23:17 +0700

    update: jurnal

---
+++ 07_parsing/Jurnal_2311104020/GlossaryItem_2311104020.js
@@ -0,0 +1,21 @@
+import { readFile } from 'fs/promises';
+
+class GlossaryItem2311104020 {
+  static async ReadJSON() {
+    const filePath = './jurnal7_3_2311104020.json';
+
+    try {
+      const data = await readFile(filePath, 'utf8');
+      const jsonData = JSON.parse(data);
+      const glossEntry = jsonData.glossary.GlossDiv.GlossList.GlossEntry;
+
+      console.log('=== Hasil Deserialisasi GlossEntry ===');
+      console.log(glossEntry);
+    } catch (err) {
+      console.error('Gagal membaca atau parsing JSON:', err);
+    }
+  }
+}
+
+// Langsung jalankan ReadJSON
+GlossaryItem2311104020.ReadJSON();
